95b56fefdc0ed3d935fd9a4f88d14bcaaee6ac1e
Pocehome <Pokehome@yandex.ru>
2024-02-20 14:20:38 +0300

    Добавьте файлы проекта.

---
+++ Monomial.h
@@ -0,0 +1,97 @@
+#include <unordered_map>
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+
+class Monomial {
+public:
+    double coef;        // monom's coefficient
+    std::unordered_map<char, int> variables;        // monomial variables and their powers
+
+    // Constructor
+    Monomial(double coef, const std::unordered_map<char, int>& variables) {
+        this->coef = coef;
+        this->variables = variables;
+    }
+
+    // Default constructor
+    Monomial() {
+        coef = 0;
+        variables = {};
+    }
+
+    // Addition operator
+    Monomial operator+(const Monomial& other) {
+        if (this->variables == other.variables) {
+            return Monomial(this->coef + other.coef, this->variables);
+        }
+        else {
+            throw std::invalid_argument("Monomials have different variables and cannot be added.");
+        }
+    }
+
+    // Subtraction operator
+    Monomial operator-(const Monomial& other) {
+        if (this->variables == other.variables) {
+            return Monomial(this->coef - other.coef, this->variables);
+        }
+        else {
+            throw std::invalid_argument("Monomials have different variables and cannot be subtracted.");
+        }
+    }
+
+    // Output operator
+    friend std::ostream& operator<<(std::ostream& os, const Monomial& monomial) {
+        os << monomial.coef;
+        for (const auto& var : monomial.variables) {
+            os << "*" << var.first << "^" << var.second;
+        }
+        return os;
+    }
+
+    // Input operator
+    friend std::istream& operator>>(std::istream& is, Monomial& monomial) {
+        std::string str;
+        is >> str;
+
+        int stage = 0;
+        std::string int_part = "";
+        char var;
+        for (int i = 0; i < str.length(); i++) {
+            if ((stage == 0) & (str[i] != '*')) {
+                int_part += str[i];
+            }
+            else if ((stage == 0) & (str[i] == '*')) {
+                monomial.coef = std::stoi(int_part);
+                int_part = "";
+                stage = 1;
+            }
+            else if (stage == 1) {
+                var = str[i];
+                stage = 2;
+            }
+            else if (stage == 2) {
+                stage = 3;
+            }
+            else if ((stage == 3) & (str[i] != '*')) {
+                int_part += str[i];
+            }
+            else if ((stage == 3) & (str[i] == '*')) {
+                monomial.variables[var] = std::stoi(int_part);
+                int_part = "";
+                stage = 1;
+            }
+            else {
+                std::cout << "Unexpected input element: " << str[i] << std::endl;
+            }
+        }
+        if ((stage == 0) & (int_part != "")) {
+            monomial.coef = std::stoi(int_part);
+        }
+        else if (stage == 3) {
+            monomial.variables[var] = std::stoi(int_part);
+        }
+        return is;
+    }
+};
---
+++ Polynomial.h
@@ -0,0 +1,117 @@
+﻿#include <stdexcept>
+#include <unordered_map>
+#include <string>
+#include <iostream>
+#include <list>
+#include <sstream>
+#include "Monomial.h"
+
+class Polynomial {
+public:
+    std::list<Monomial> monoms;
+
+    Polynomial() : monoms({}) {}
+
+    void push(const Monomial& monom) {
+        for (auto it = monoms.begin(); it != monoms.end(); ++it) {
+            if (it->variables == monom.variables) {
+                it->coef += monom.coef;
+                if (it->coef == 0) {
+                    monoms.erase(it);
+                }
+                return;
+            }
+        }
+        if (monom.coef != 0) {
+            monoms.push_back(monom);
+        }
+    }
+
+
+    // Метод для проверки корректности входной строки
+    bool isValidInput(const std::string& str) {
+        for (char c : str) {
+            if (!std::isdigit(c) && c != '+' && c != '-' && c != '*' && c != '^' && !std::isalpha(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Оператор сложения
+    Polynomial operator+(const Polynomial& other) {
+        Polynomial result = *this;
+        for (const auto& monom : other.monoms) {
+            result.push(monom);
+        }
+        return result;
+    }
+
+    // Оператор вычитания
+    Polynomial operator-(const Polynomial& other) {
+        Polynomial result = *this;
+        for (auto& monom : other.monoms) {
+            Monomial negative_monom(monom);
+            negative_monom.coef = -monom.coef;
+            result.push(negative_monom);
+        }
+        return result;
+    }
+
+    // Оператор вывода
+    friend std::ostream& operator<<(std::ostream& os, const Polynomial& Polynomial) {
+        for (std::list<Monomial>::const_iterator it = Polynomial.monoms.begin(); it != Polynomial.monoms.end(); ++it) {
+            if (it != Polynomial.monoms.begin()) {
+                os << " + ";
+            }
+            os << *it;
+        }
+        return os;
+    }
+
+    // Оператор ввода
+    friend std::istream& operator>>(std::istream& is, Polynomial& Polynomial) {
+        std::string str;
+        is >> str;
+        if (!Polynomial.isValidInput(str)) {
+            throw std::invalid_argument("Некорректный ввод");
+        }
+
+        std::string monom_str = "";
+
+        if (str[0] == '-') {
+            monom_str = "-";
+            str = str.substr(1);
+        }
+
+        for (char symbol : str) {
+
+            if (symbol == '-') {
+                Monomial monom;
+                std::istringstream monom_iss(monom_str);
+                monom_iss >> monom;
+                Polynomial.push(monom);
+
+                monom_str = "-";
+            }
+
+            else if (symbol == '+') {
+                Monomial monom;
+                std::istringstream monom_iss(monom_str);
+                monom_iss >> monom;
+                Polynomial.push(monom);
+
+                monom_str = "";
+            }
+
+            else {
+                monom_str += symbol;
+            }
+        }
+
+        Monomial monom;
+        std::istringstream monom_iss(monom_str);
+        monom_iss >> monom;
+        Polynomial.push(monom);
+    }
+};
